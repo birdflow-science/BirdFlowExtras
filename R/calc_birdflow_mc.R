64522ba34ab9935e5bea282b9ec52423212e79f7
Adriaan Dokter <adriaandokter@gmail.com>
2025-04-24 13:10:18 -0400

    add more vectorization

--- R/calc_birdflow_mc.R
+++ R/calc_birdflow_mc.R
@@ -63,12 +63,11 @@ calc_birdflow_mc <- function(bf, ...) {
   # calculate MC
   MC=0
   print("Starting MC calculation...")
-  for (i in 1:dim(origin_dist)[1]){
-    print(paste(i,"/",dim(origin_dist)[1]))
-    for(j in 1:dim(target_dist)[1]){
-      Delta_MC=psi_abun[i,j]*((origin_dist[,i] - mu_D) / sd_D) %*% psi_abun %*% ((target_dist[j,] - mu_V) / sd_V)
-      MC=MC+Delta_MC
-    }
+
+  for(j in 1:dim(target_dist)[1]){
+    print(paste(j,"/",dim(origin_dist)[1]))
+    Delta_MC=(psi_abun[,j] %*% ((origin_dist - mu_D) / sd_D)) %*% psi_abun %*% ((target_dist[j,] - mu_V) / sd_V)
+    MC=MC+Delta_MC
   }
 
   return(MC)
